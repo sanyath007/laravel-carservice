0f6c73d87937889de64cd861b1bf9acacc8a145d
Sanya  Thammawong <sanyath007@gmail.com>
2018-02-23 02:31:39 +0700

    Move model class User0 to App/Models

--- app/Models/User0.php
+++ app/Models/User0.php
@@ -1,10 +1,11 @@
 <?php
 
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User0 extends Authenticatable
+class User extends Authenticatable
 {
     use Notifiable;
 

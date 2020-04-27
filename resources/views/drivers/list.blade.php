254e931cd899b9e41d3fe01793754e0cf7c0a63a
sanyath007 <sanyath007@gmail.com>
2020-04-27 16:25:44 +0700

    Re-designed home page and driver list page 1st day

--- resources/views/drivers/list.blade.php
+++ resources/views/drivers/list.blade.php
@@ -46,6 +46,164 @@
         </table>
       </div>
     </div>
-  </div>
-</div>
+
+    <div class="col-12 col-md-6 col-lg-3">
+      <div class="cnt-block equal-hight" style="height: 349px;">
+          <figure>
+              <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+          </figure>
+          <h3><a href="http://www.webcoderskull.com/">นายอาราม ราชภักดี</a></h3>
+          <p>Freelance Web Developer</p>
+          <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายคฑาวุธ สิงห์แจ่ม</a></h3>
+            <p>Freelance Web Developer</p>
+            <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายคงฤทธิ์ แสนพิมพ์</a></h3>
+            <p>Freelance Web Developer</p>
+            <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายสายันต์ คงสม</a></h3>
+            <p>Freelance Web Developer</p>
+            <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายณัฐพล ไชยสระน้อย</a></h3>
+            <p>Freelance Web Developer</p>
+            <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div> -->
+
+    <!-- <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายสมโภชน์ พิมพิค่อ</a></h3>
+            <p>Freelance Web Developer</p>
+            <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div> -->
+
+    <!-- <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายพนมพร เนตรสูงเนิน</a></h3>
+            <p>Freelance Web Developer</p>
+            <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div> -->
+
+    <!-- <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายสายันต์ คงสม</a></h3>
+            <p>Freelance Web Developer</p>
+            <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div> -->
+
+  </div><!-- /. Col-12 -->
+</div><!-- Row -->
+
+<style>
+.cnt-block{ 
+   float:left; 
+   width:100%; 
+   background:#fff; 
+   padding:30px 20px; 
+   text-align:center; 
+   border:2px solid #d5d5d5;
+   margin: 0 0 28px;
+}
+.cnt-block figure{
+   width:148px; 
+   height:148px; 
+   border-radius:100%; 
+   display:inline-block;
+   margin-bottom: 15px;
+}
+.cnt-block img{ 
+   width:148px; 
+   height:148px; 
+   border-radius:100%; 
+}
+.cnt-block .follow-us{
+	margin:20px 0 0;
+}
+.cnt-block .follow-us li{ 
+    display:inline-block; 
+	width:auto; 
+	margin:0 5px;
+}
+.cnt-block .follow-us li .fa{ 
+   font-size:24px; 
+   color:#767676;
+}
+</style>
+
 @endsection

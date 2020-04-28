7c1970536cc78b3415261dcba4cb695459287846
sanyath007 <sanyath007@gmail.com>
2020-04-28 16:43:40 +0700

    Re-design driver list page 2nd day

--- resources/views/drivers/list.blade.php
+++ resources/views/drivers/list.blade.php
@@ -23,16 +23,19 @@
             <th style="width: 13%; text-align: center;">เลขประจำตัวประชาชน</th>
             <th>ชื่อ-สกุล</th>
             <th style="width: 10%; text-align: center;">เบอร์ติดต่อ</th>
-            <th style="width: 12%; text-align: center;">ผ่านการอบรมเมื่อ</th>
+            <th style="width: 12%; text-align: center;">ผ่านการอบรม พขร. เมื่อ</th>
+            <th style="width: 12%; text-align: center;">ผ่านการอบรม EMR เมื่อ</th>
             <th style="width: 6%; text-align: center;">Actions</th>
           </tr>
+          {{ $row = 0 }}
           @foreach($drivers as $driver)
           <tr>
-            <td style=" text-align: center;">{{ $driver->driver_id }}</td>
+            <td style=" text-align: center;">{{ ++$row }}</td>
             <td style=" text-align: center;">{{ $driver->person_id }}</td>
             <td>{{ $driver->description }}</td>
             <td style=" text-align: center;">{{ $driver->person->person_tel }}</td>
             <td style=" text-align: center;">{{ $driver->certificated_date }}</td>
+            <td style=" text-align: center;">{{ $driver->emr_sdate }}</td>
             <td style=" text-align: center;">
               <a href="{{$driver->driver_id}}" class="btn btn-warning btn-xs">
                 <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
@@ -47,118 +50,126 @@
       </div>
     </div>
 
+    <!-- #=============== New List ===============# -->
     <div class="col-12 col-md-6 col-lg-3">
-      <div class="cnt-block equal-hight" style="height: 349px;">
-          <figure>
-              <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
-          </figure>
-          <h3><a href="http://www.webcoderskull.com/">นายอาราม ราชภักดี</a></h3>
-          <p>Freelance Web Developer</p>
-          <ul class="follow-us clearfix">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="{{ asset('/uploads/drivers/no-image-300x300.jpg') }}" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายอาราม ราชภักดี</a></h3>
+            <p>พนักงานขับรถยนต์</p>
+            <p>โทร. 0866509103</p>
+            <!-- <ul class="follow-us clearfix">
                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
+            </ul> -->
       </div>
     </div>
 
     <div class="col-12 col-md-6 col-lg-3">
         <div class="cnt-block equal-hight" style="height: 349px;">
             <figure>
-                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+                <img src="{{ asset('/uploads/drivers/no-image-300x300.jpg') }}" class="img-responsive" alt="">
             </figure>
             <h3><a href="http://www.webcoderskull.com/">นายคฑาวุธ สิงห์แจ่ม</a></h3>
-            <p>Freelance Web Developer</p>
-            <ul class="follow-us clearfix">
+            <p>พนักงานขับรถยนต์</p>
+            <p>โทร. 0828702283</p>
+            <!-- <ul class="follow-us clearfix">
                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
+            </ul> -->
         </div>
     </div>
 
     <div class="col-12 col-md-6 col-lg-3">
         <div class="cnt-block equal-hight" style="height: 349px;">
             <figure>
-                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+                <img src="{{ asset('/uploads/drivers/no-image-300x300.jpg') }}" class="img-responsive" alt="">
             </figure>
             <h3><a href="http://www.webcoderskull.com/">นายคงฤทธิ์ แสนพิมพ์</a></h3>
-            <p>Freelance Web Developer</p>
-            <ul class="follow-us clearfix">
+            <p>พนักงานขับรถยนต์</p>
+            <p>โทร. 0854116068</p>
+            <!-- <ul class="follow-us clearfix">
                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
+            </ul> -->
         </div>
     </div>
 
     <div class="col-12 col-md-6 col-lg-3">
         <div class="cnt-block equal-hight" style="height: 349px;">
             <figure>
-                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+                <img src="{{ asset('/uploads/drivers/3201000048759.jpg') }}" class="img-responsive" alt="">
             </figure>
             <h3><a href="http://www.webcoderskull.com/">นายสายันต์ คงสม</a></h3>
-            <p>Freelance Web Developer</p>
-            <ul class="follow-us clearfix">
+            <p>พนักงานขับรถยนต์</p>
+            <p>โทร. 0930190899</p>
+            <!-- <ul class="follow-us clearfix">
                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
+            </ul> -->
+        </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="{{ asset('/uploads/drivers/no-image-300x300.jpg') }}" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายณัฐพล ไชยสระน้อย</a></h3>
+            <p>พนักงานขับรถยนต์</p>
+            <p>โทร. 0981215165</p>
+            <!-- <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul> -->
+        </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="{{ asset('/uploads/drivers/no-image-300x300.jpg') }}" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายสมโภชน์ พิมพิค่อ</a></h3>
+            <p>พนักงานขับรถยนต์</p>
+            <p>โทร. 0986230526</p>
+            <!-- <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul> -->
+        </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-3">
+        <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure>
+                <img src="{{ asset('/uploads/drivers/no-image-300x300.jpg') }}" class="img-responsive" alt="">
+            </figure>
+            <h3><a href="http://www.webcoderskull.com/">นายพนมพร เนตรสูงเนิน</a></h3>
+            <p>พนักงานขับรถยนต์</p>
+            <p>โทร. 0973352538</p>
+            <!-- <ul class="follow-us clearfix">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul> -->
         </div>
     </div>
 
     <!-- <div class="col-12 col-md-6 col-lg-3">
         <div class="cnt-block equal-hight" style="height: 349px;">
             <figure>
-                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
-            </figure>
-            <h3><a href="http://www.webcoderskull.com/">นายณัฐพล ไชยสระน้อย</a></h3>
-            <p>Freelance Web Developer</p>
-            <ul class="follow-us clearfix">
-                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-    </div> -->
-
-    <!-- <div class="col-12 col-md-6 col-lg-3">
-        <div class="cnt-block equal-hight" style="height: 349px;">
-            <figure>
-                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
-            </figure>
-            <h3><a href="http://www.webcoderskull.com/">นายสมโภชน์ พิมพิค่อ</a></h3>
-            <p>Freelance Web Developer</p>
-            <ul class="follow-us clearfix">
-                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-    </div> -->
-
-    <!-- <div class="col-12 col-md-6 col-lg-3">
-        <div class="cnt-block equal-hight" style="height: 349px;">
-            <figure>
-                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
-            </figure>
-            <h3><a href="http://www.webcoderskull.com/">นายพนมพร เนตรสูงเนิน</a></h3>
-            <p>Freelance Web Developer</p>
-            <ul class="follow-us clearfix">
-                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-    </div> -->
-
-    <!-- <div class="col-12 col-md-6 col-lg-3">
-        <div class="cnt-block equal-hight" style="height: 349px;">
-            <figure>
-                <img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt="">
+                <img src="{{ asset('/uploads/drivers/3201000048759.jpg') }}" class="img-responsive" alt="">
             </figure>
             <h3><a href="http://www.webcoderskull.com/">นายสายันต์ คงสม</a></h3>
-            <p>Freelance Web Developer</p>
+            <p>พนักงานขับรถยนต์</p>
             <ul class="follow-us clearfix">
                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
@@ -191,6 +202,9 @@
    width:148px; 
    height:148px; 
    border-radius:100%; 
+}
+.cnt-block p{ 
+   margin:1px;
 }
 .cnt-block .follow-us{
 	margin:20px 0 0;

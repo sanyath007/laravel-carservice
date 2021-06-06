ffc9f532078e15246c6376a51dedb163066343d0
sanyath007 <sanyath007@gmail.com>
2021-06-06 15:04:10 +0700

    Change namespace of ReservePassenger model in ReservePassengerController

--- app/Http/Controllers/ReservePassengerController.php
+++ app/Http/Controllers/ReservePassengerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\ReservePassenger;
+use App\Models\ReservePassenger;
 use App\Position;
 use App\User;
 
@@ -25,27 +25,25 @@ class ReservePassengerController extends Controller
 
         $users = [];
         foreach ($passengers as $passenger) {
-	        $user = User::where(['person_id' => $passenger->person_id])
-	        			->with('position')
-	        			->with('academic')
+            $user = User::where(['person_id' => $passenger->person_id])
+                        ->with('position')
+                        ->with('academic')
                         ->with('ward')
-	        			->first();
+                        ->first();
 
-	    	// var_dump($user);
-	    	$academic = ($user->ac_id > 0) ? $user->academic->ac_name : '';
+            $academic = ($user->ac_id > 0) ? $user->academic->ac_name : '';
 
-	       	array_push($users, [
-	       		'id' => $user->person_id, 
-	       		'name' => $user->person_firstname. ' ' .$user->person_lastname,
-	       		'position' => $user->position->position_name. '' .$academic,
+            array_push($users, [
+                'id' => $user->person_id, 
+                'name' => $user->person_firstname. ' ' .$user->person_lastname,
+                'position' => $user->position->position_name. '' .$academic,
                 'ward' => $user->ward->ward_name,
-	       	]);
+            ]);
         }
-	    
-	    // var_dump($users);
+
         return $data = [
-        	$passengers, 
-        	$users
+            $passengers, 
+            $users
         ];
     }
 }

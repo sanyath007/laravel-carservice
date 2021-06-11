fc2cd90b9a4cba729a927ab5964106c00c46a227
sanyath007 <sanyath007@gmail.com>
2021-06-11 21:47:23 +0700

    Editted store and update method of VehicleController by using uploadFile from helpers function to upload vehicle image

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -97,34 +97,21 @@ class VehicleController extends Controller
     }
 
     public function detail($id) 
-    {
-        // $vehicle = Vehicle::where(['vehicle_id' => $id])
-        //                         ->with('cate')
-        //                         ->with('type')
-        //                         ->with('method')
-        //                         ->with('manufacturer')
-        //                         ->with('changwat')
-        //                         ->with('vender')
-        //                         ->with('fuel')
-        //                         ->with('taxactived')                          
-        //                         ->with('insactived')                          
-        //                         ->with('actsactived')
-        //                         ->first();
-        // var_dump($vehicle);         
+    {        
         return view('vehicles.detail', [
             'vehicle' => Vehicle::where(['vehicle_id' => $id])
-                                ->with('cate')
-                                ->with('type')
-                                ->with('method')
-                                ->with('manufacturer')
-                                ->with('changwat')
-                                ->with('vender')
-                                ->with('fuel')
-                                ->with('taxactived')                          
-                                ->with('insactived')                          
-                                ->with('actsactived')
-                                ->with('mileage')
-                                ->first()
+                            ->with('cate')
+                            ->with('type')
+                            ->with('method')
+                            ->with('manufacturer')
+                            ->with('changwat')
+                            ->with('vender')
+                            ->with('fuel')
+                            ->with('taxactived')                          
+                            ->with('insactived')                          
+                            ->with('actsactived')
+                            ->with('mileage')
+                            ->first()
         ]);
     }
 
@@ -146,13 +133,6 @@ class VehicleController extends Controller
         /** Current date */
         $d = new \DateTime(date('Y-m-d H:i:s'));
         $diffHours = new \DateInterval('PT7H');
-
-        /** Upload attach file */
-        $filename = '';
-        if ($file = $req->file('attachfile')) {
-            $filename = $file->getClientOriginalName();
-            $file->move('uploads', $filename);
-        }
 
         $newVehicle = new Vehicle();
         $newVehicle->vehicle_no = $req['vehicle_no'];   
@@ -186,6 +166,12 @@ class VehicleController extends Controller
         $newVehicle->remark = $req['remark'];
         $newVehicle->status = '1';
 
+        /** Upload attach file */
+        $attachfile = uploadFile($req->file('attachfile'), 'uploads/vehicles/thumbnails');
+        if ($attachfile != '') {
+            $newVehicle->attachfile = $attachfile;
+        }
+
         if ($newVehicle->save()) {                            
             return redirect('vehicles/list');
         }
@@ -211,39 +197,45 @@ class VehicleController extends Controller
         $d = new \DateTime(date('Y-m-d H:i:s'));
         $diffHours = new \DateInterval('PT7H');
 
-        $newVehicle = Vehicle::find($id);
-        $newVehicle->vehicle_no = $req['vehicle_no'];   
-        $newVehicle->vehicle_cate = $req['vehicle_cate'];
-        $newVehicle->vehicle_type = $req['vehicle_type'];
-        $newVehicle->manufacturer_id = $req['manufacturer'];
-        $newVehicle->model = $req['model'];
-        $newVehicle->color = $req['color'];
-        $newVehicle->year = $req['year'];
-        $newVehicle->capacity = $req['capacity'];
-        $newVehicle->fuel_type = $req['fuel_type'];
-        $newVehicle->chassis_no = $req['chassis_no'];
-        $newVehicle->engine_no = $req['engine_no'];
-        $newVehicle->reg_no = $req['reg_no'];
-        $newVehicle->reg_chw = $req['reg_chw'];
-        $newVehicle->reg_date = $req['reg_date'];
-        $newVehicle->vender_id = $req['vender'];
-        $newVehicle->purchased_method = $req['method'];
-        $newVehicle->purchased_date = $req['purchased_date'];
-        $newVehicle->date_in = $d->add($diffHours);
+        $vehicle = Vehicle::find($id);
+        $vehicle->vehicle_no = $req['vehicle_no'];   
+        $vehicle->vehicle_cate = $req['vehicle_cate'];
+        $vehicle->vehicle_type = $req['vehicle_type'];
+        $vehicle->manufacturer_id = $req['manufacturer'];
+        $vehicle->model = $req['model'];
+        $vehicle->color = $req['color'];
+        $vehicle->year = $req['year'];
+        $vehicle->capacity = $req['capacity'];
+        $vehicle->fuel_type = $req['fuel_type'];
+        $vehicle->chassis_no = $req['chassis_no'];
+        $vehicle->engine_no = $req['engine_no'];
+        $vehicle->reg_no = $req['reg_no'];
+        $vehicle->reg_chw = $req['reg_chw'];
+        $vehicle->reg_date = $req['reg_date'];
+        $vehicle->vender_id = $req['vender'];
+        $vehicle->purchased_method = $req['method'];
+        $vehicle->purchased_date = $req['purchased_date'];
+        $vehicle->date_in = $d->add($diffHours);
 
         /** Accessories */
-        $newVehicle->cam_front = $req['cam_front'] ? $req['cam_front'] : 0;
-        $newVehicle->cam_back = $req['cam_back'] ? $req['cam_back'] : 0;
-        $newVehicle->cam_driver = $req['cam_driver'] ? $req['cam_driver'] : 0;
-        $newVehicle->gps = $req['gps'] ? $req['gps'] : 0;
-        $newVehicle->siren = $req['siren'] ? $req['siren'] : 0;
-        $newVehicle->light = $req['light'] ? $req['light'] : 0;
-        $newVehicle->radio_com = $req['radio_com'] ? $req['radio_com'] : 0;
-        $newVehicle->tele_med = $req['tele_med'] ? $req['tele_med'] : 0;
-        $newVehicle->remark = $req['remark'];
-        $newVehicle->status = $req['status'];
+        $vehicle->cam_front = $req['cam_front'] ? $req['cam_front'] : 0;
+        $vehicle->cam_back = $req['cam_back'] ? $req['cam_back'] : 0;
+        $vehicle->cam_driver = $req['cam_driver'] ? $req['cam_driver'] : 0;
+        $vehicle->gps = $req['gps'] ? $req['gps'] : 0;
+        $vehicle->siren = $req['siren'] ? $req['siren'] : 0;
+        $vehicle->light = $req['light'] ? $req['light'] : 0;
+        $vehicle->radio_com = $req['radio_com'] ? $req['radio_com'] : 0;
+        $vehicle->tele_med = $req['tele_med'] ? $req['tele_med'] : 0;
+        $vehicle->remark = $req['remark'];
+        $vehicle->status = $req['status'];
 
-        if ($newVehicle->save()) {                            
+        /** Upload attach file */
+        $attachfile = uploadFile($req->file('attachfile'), 'uploads/vehicles/thumbnails');
+        if ($attachfile != '') {
+            $vehicle->attachfile = $attachfile;
+        }
+
+        if ($vehicle->save()) {                            
             return redirect('vehicles/list');
         }
     }
@@ -252,16 +244,16 @@ class VehicleController extends Controller
     {
         return [
             'vehicles' => Vehicle::where(['status' => '1'])
-                                ->with('cate')
-                                ->with('type')
-                                ->with('method')
-                                ->with('manufacturer')
-                                ->with('changwat')
-                                ->with('vender')
-                                ->with('fuel')                                
-                                ->orderBy('vehicle_type', 'ASC')
-                                ->orderBy('vehicle_cate', 'ASC')
-                                ->paginate(10)
+                            ->with('cate')
+                            ->with('type')
+                            ->with('method')
+                            ->with('manufacturer')
+                            ->with('changwat')
+                            ->with('vender')
+                            ->with('fuel')                                
+                            ->orderBy('vehicle_type', 'ASC')
+                            ->orderBy('vehicle_cate', 'ASC')
+                            ->paginate(10)
         ];
     }
 }
